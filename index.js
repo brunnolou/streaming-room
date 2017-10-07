32f972dbfb18c31112f2365816cf6921d755c26a
Bruno Lourenco <brunnolou@gmail.com>
2017-10-07 19:34:42 +0100

    Add watching count and style

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const RtmpServer = require('rtmp-server');
 const rtmpServer = new RtmpServer();
- 
+
 rtmpServer.on('error', err => {
   throw err;
 });
- 
+
+let nowWatching = 0;
+
+const renderWatchCount = count => {
+  console.log('┌──────────────────────┬───┐');
+  console.log('│ NOW WATCHING:        │', count, '│');
+  console.log('├──────────────────────┴───┤');
+  console.log('│', new Date().toISOString().replace('T', ' '), '│');
+  console.log('└──────────────────────────┘');
+}
+
 rtmpServer.on('client', client => {
-  //client.on('command', command => { 
-  //  console.log(command.cmd, command); 
-  //}); 
- 
+  //client.on('command', command => {
+  //  console.log(command.cmd, command);
+  //});
+
   client.on('connect', () => {
-     console.log('connect', client.app);
   });
-  
+
   client.on('play', ({ streamName }) => {
-    console.log('PLAY', streamName);
+    nowWatching++;
+    console.log('Play');
+    renderWatchCount(nowWatching);
   });
-  
-  client.on('publish', ({ streamName }) => {
-    console.log('PUBLISH', streamName);
-  });
-  
+
   client.on('stop', () => {
-    console.log('client disconnected');
+    nowWatching--;
+    console.log('Disconnected');
+    renderWatchCount(nowWatching);
+  });
+
+  client.on('publish', ({ streamName }) => {
+    console.log('Publish - ', streamName, ' ', new Date().toISOString());
+    console.log('--------------------------------');
   });
 });
- 
+
 rtmpServer.listen(1935);
