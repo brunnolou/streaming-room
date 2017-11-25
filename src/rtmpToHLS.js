191d102559d96b43dfd9586688210f0269f419fa
Bruno Lourenço <brunolou@gmail.com>
2017-11-25 22:22:58 +0000

    Fix missing slashes on rtmp address

--- src/rtmpToHLS.js
+++ src/rtmpToHLS.js
@@ -2,7 +2,7 @@ var ffmpeg = require("fluent-ffmpeg");
 const { exec } = require('child_process');
 
 const rtmpToHLS = (streamName, path) => {
-  exec('ffmpeg -v verbose -i rtmp:localhost/live/live -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 public/videos/output.m3u8', 
+  exec('ffmpeg -v verbose -i rtmp://localhost/live/live -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 public/videos/output.m3u8', 
   (err, stdout, stderr) => {
 	  if (err) {
 		// node couldn't execute the command
