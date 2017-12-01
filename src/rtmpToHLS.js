1594e08db2e75993ed238de8e09f4033623f3361
Bruno Lourenço <brunolou@gmail.com>
2017-12-01 12:10:57 +0000

    Remove ffmpeg command console log

--- src/rtmpToHLS.js
+++ src/rtmpToHLS.js
@@ -30,7 +30,6 @@ const rtmpToHLS = () => {
     path.join(videosPath, output);
 
     // ffmpeg -v verbose -i rtmp:localhost/live/live -c:v libx264 -c:a aac -ac 1 -strict -2 -crf 18 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 public/videos/output.m3u8
-    console.log('command: ', command);
     exec(command, (err, stdout, stderr) => {
       if (err) {
         console.log("ffmpeg error: ", err);
