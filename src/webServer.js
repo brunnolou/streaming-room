3824e48c0d3ddb3bbfab6aa2cecb91c484465e6a
Yves <54778078+yves0147@users.noreply.github.com>
2019-09-27 15:03:01 +0200

    i18n - Add German

--- src/webServer.js
+++ src/webServer.js
@@ -47,7 +47,7 @@ function webServer(app, server) {
 
   // i18n
   i18n.configure({
-    locales: ['pt', 'en'],
+    locales: ['pt', 'de', 'en'],
     queryParameter: 'lang',
     directory: path.join(__dirname, 'locales'),
   });
